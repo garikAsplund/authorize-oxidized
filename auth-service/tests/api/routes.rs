8d55753da336169df8f1f345d5bbbc58bedcdd20
Garik Asplund <garik.asplund@gmail.com>
2024-05-13 12:58:35 -0700

    Add auth-service directory

---
+++ auth-service/tests/api/routes.rs
@@ -0,0 +1,9 @@
+use crate::helpers::TestApp;
+
+// Tokio's test macro is use to run the test in an async environment
+
+// TODO: Implement tests for all other routes (signup, login, logout, verify-2fa, and verify-token)
+// For now, simply assert that each route returns a 200 HTTP status code.
+
+
+
