8526a973007904a6509c287de34d91bcc7366829
Garik Asplund <garik.asplund@gmail.com>
2024-05-13 11:45:54 -0700

    modularized routes

--- auth-service/src/lib.rs
+++ auth-service/src/lib.rs
@@ -1,6 +1,8 @@
 use axum::{http::StatusCode, response::IntoResponse, routing::post, serve::Serve, Router};
 use std::error::Error;
 use tower_http::services::ServeDir;
+use crate::routes::{signup, login, logout, verify_2fa, verify_token};
+pub mod routes;
 
 // This struct encapsulates our application-related logic.
 pub struct Application {
@@ -35,24 +37,4 @@ impl Application {
         println!("listening on {}", &self.address);
         self.server.await
     }
-}
-
-async fn signup() -> impl IntoResponse {
-    StatusCode::OK.into_response()
-}
-
-async fn login() -> impl IntoResponse {
-    StatusCode::OK.into_response()
-}
-
-async fn logout() -> impl IntoResponse {
-    StatusCode::OK.into_response()
-}
-
-async fn verify_2fa() -> impl IntoResponse {
-    StatusCode::OK.into_response()
-}
-
-async fn verify_token() -> impl IntoResponse {
-    StatusCode::OK.into_response()
 }
