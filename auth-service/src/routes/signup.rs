3366a834286832b5a3467dc3632a8555cdbb49a2
Garik Asplund <garik.asplund@gmail.com>
2024-05-23 16:02:19 -0700

    sprint 3 task 3

--- auth-service/src/routes/signup.rs
+++ auth-service/src/routes/signup.rs
@@ -1,26 +1,43 @@
 use axum::{extract::State, http::StatusCode, response::IntoResponse, Json};
 use serde::{Deserialize, Serialize};
 
-use crate::{app_state::AppState, domain::User};
+use crate::{
+    app_state::AppState,
+    domain::{AuthAPIError, User},
+};
 
 pub async fn signup(
-    // TODO: Use Axum's state extractor to pass in AppState
     State(state): State<AppState>,
     Json(request): Json<SignupRequest>,
-) -> impl IntoResponse {
-    // Create a new `User` instance using data in the `request`
-    let user = User::new(request.email, request.password, request.requires_2fa);
+) -> Result<impl IntoResponse, AuthAPIError> {
+    let email = request.email;
+    let password = request.password;
+
+    // TODO: early return AuthAPIError::InvalidCredentials if:
+    // - email is empty or does not contain '@'
+    // - password is less than 8 characters
+    if email.is_empty() || password.len() < 8 || !email.contains('@') {
+        return Err(AuthAPIError::InvalidCredentials);
+    }
+
+    let user = User::new(email, password, request.requires_2fa);
 
     let mut user_store = state.user_store.write().await;
 
-    // TODO: Add `user` to the `user_store`. Simply unwrap the returned `Result` enum type for now.
-    user_store.add_user(user).unwrap();
+    // TODO: early return AuthAPIError::UserAlreadyExists if email exists in user_store.
+    if user_store.get_user(&user.email).is_ok() {
+        return Err(AuthAPIError::UserAlreadyExists);
+    }
 
+    // TODO: instead of using unwrap, early return AuthAPIError::UnexpectedError if add_user() fails.
+    if user_store.add_user(user).is_err() {
+        return Err(AuthAPIError::UnexpectedError);
+    }
     let response = Json(SignupResponse {
         message: "User created successfully!".to_string(),
     });
 
-    (StatusCode::CREATED, response)
+    Ok((StatusCode::CREATED, response))
 }
 
 #[derive(Serialize)]
