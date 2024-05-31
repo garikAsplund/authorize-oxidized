6b5c952e4f4a4cdeb4222f92eae86bff935be413
Garik Asplund <garik.asplund@gmail.com>
2024-05-31 16:16:28 -0700

    sprint 4 task 5

--- auth-service/src/routes/verify_token.rs
+++ auth-service/src/routes/verify_token.rs
@@ -1,5 +1,17 @@
-use axum::{http::StatusCode, response::IntoResponse};
+use axum::{http::StatusCode, response::IntoResponse, Json};
+use serde::Deserialize;
 
-pub async fn verify_token() -> impl IntoResponse {
-    StatusCode::OK.into_response()
+use crate::{domain::AuthAPIError, utils::auth::validate_token};
+
+pub async fn verify_token( Json(request): Json<VerifyTokenRequest>,
+) -> Result<impl IntoResponse, AuthAPIError> {
+    match validate_token(&request.token).await {
+        Ok(_) => return Ok(StatusCode::OK.into_response()),
+        Err(_) => return Err(AuthAPIError::InvalidToken),
+    };
+}
+
+#[derive(Deserialize)]
+pub struct VerifyTokenRequest {
+    pub token: String,
 }
